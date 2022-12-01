5f486848999ac52daff66c1627cfc531076a3cbd
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-12-01 16:40:18 +0100

    add function getFirstLetter and add Btn listener
    split()

--- app/src/main/java/com/example/insightfuture/MainActivity.kt
+++ app/src/main/java/com/example/insightfuture/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.insightfuture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import com.example.insightfuture.databinding.ActivityMainBinding
 
@@ -12,19 +14,34 @@ class MainActivity : AppCompatActivity() {
     private lateinit var name : EditText
     private lateinit var surname : EditText
     private lateinit var bornPlace : EditText
+    private lateinit var searchBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         question = binding.question
         name = binding.name
         surname = binding.surname
         bornPlace = binding.bornPlace
-
+        searchBtn = binding.searchBtn
+        
+        searchBtn.setOnClickListener{
+            getFirstLetter(question.text.toString())
+        }
 
 
     }
+
+
+    private fun getFirstLetter(stringa : String): Array<String> {
+        val delim = " "
+        val arr = stringa.split(delim).toTypedArray()
+        Log.d("array", arr.toString())
+        return arr
+    }
+
+
 }

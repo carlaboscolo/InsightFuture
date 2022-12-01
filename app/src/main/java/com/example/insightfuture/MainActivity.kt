a453f25542abe61a3a11a6206ddda74ad6e62a29
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-12-01 16:04:28 +0100

    add binding and variable in mainActivity

--- app/src/main/java/com/example/insightfuture/MainActivity.kt
+++ app/src/main/java/com/example/insightfuture/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.insightfuture
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.example.insightfuture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var question : EditText
+    private lateinit var name : EditText
+    private lateinit var surname : EditText
+    private lateinit var bornPlace : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        question = binding.question
+        name = binding.name
+        surname = binding.surname
+        bornPlace = binding.bornPlace
+
 
 
     }

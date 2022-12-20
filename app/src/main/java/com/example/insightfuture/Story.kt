96e49daebc6bfd1c6bf59fc2bf9bd53ee2136877
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-12-20 14:38:30 +0100

    add back button in story, archive and package page

--- app/src/main/java/com/example/insightfuture/Story.kt
+++ app/src/main/java/com/example/insightfuture/Story.kt
@@ -2,18 +2,26 @@ package com.example.insightfuture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.insightfuture.databinding.ActivityMenuBinding
+import android.widget.Button
 import com.example.insightfuture.databinding.ActivityStoryBinding
 
 class Story : AppCompatActivity() {
 
     private lateinit var binding : ActivityStoryBinding
+    private lateinit var backBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_story)
         binding = ActivityStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        backBtn = binding.backBtn
+
+        backBtn.setOnClickListener{
+            finish()
+        }
+
 
     }
 }

a44918f226c010466b9c0cb2020e42fb06e13d87
Carla <100995659+carlaboscolo@users.noreply.github.com>
2022-12-26 23:13:31 +0100

    added search binding

--- app/src/main/java/com/example/insightfuture/archive/ArchiveActivity.kt
+++ app/src/main/java/com/example/insightfuture/archive/ArchiveActivity.kt
@@ -3,17 +3,23 @@ package com.example.insightfuture.archive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.insightfuture.ArchiveAdapter
 import com.example.insightfuture.databinding.ActivityArchiveBinding
 import com.example.roomdatabase.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ArchiveActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityArchiveBinding
     private lateinit var backBtn : Button
+    private lateinit var searchBtn : Button
+    private lateinit var searchText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +28,21 @@ class ArchiveActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         backBtn = binding.backBtn
+        searchBtn = binding.backBtn
+        searchText = binding.searchBar
 
         backBtn.setOnClickListener{
             finish()
+        }
+
+        searchBtn.setOnClickListener {
+
+            val searchQuery = searchText.text.toString().toLowerCase()
+
+            if(searchQuery.isNotEmpty()){
+               readData()
+            }
+
         }
 
     }
@@ -44,5 +62,35 @@ class ArchiveActivity : AppCompatActivity() {
 
         }
     }
+
+
+
+/* private suspend fun displayData(student: Student){
+  /*
+ ithContext(Dispatchers.Main){
+       binding.tvFirstName.text = student.firstName
+       binding.tvLastName.text = student.lastName
+       binding.tvRollNo.text = student.rollNo.toString()
+   }
+
+} */
+
+ */
+
+
+private fun readData(){
+  /* val rollNo = binding.etRollNoRead.text.toString()
+
+   if(rollNo.isNotEmpty()){
+       lateinit var student: Student
+
+       GlobalScope.launch {
+           student = appDB.studentDao().findByRoll(rollNo.toInt())
+           displayData(student)
+       }
+
+   }
+   */
+}
 
 }
--- app/src/main/java/com/example/insightfuture/roomDatabase/SibillaDao.kt
+++ app/src/main/java/com/example/insightfuture/roomDatabase/SibillaDao.kt
@@ -8,8 +8,11 @@ interface SibillaDao {
     @Query("SELECT * FROM sibilla_table")
     suspend fun getAll() :  List<SibillaDatabase>
 
-    @Query("SELECT * FROM sibilla_table WHERE data LIKE :data LIMIT 1")
-    suspend fun findByData(data : String) : SibillaDatabase
+    // @Query("SELECT * FROM sibilla_table WHERE data LIKE :data LIMIT 1")
+    // suspend fun findByData(data : String) : SibillaDatabase
+
+    // @Query("SELECT * FROM sibilla_table WHERE name, surname, bornPlace, data, question LIKE : query% ")
+    // suspend fun findByQuery(query : String) : SibillaDatabase
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(sibilla: SibillaDatabase)
